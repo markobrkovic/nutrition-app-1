2613165f525bac195b5796017866506a6e65f035
“WebDevManuel” <donmanuel2005@web.de>
2021-10-08 10:39:56 +0200

    Add weekly-progress-tracker

---
+++ src/components/weekly-progress/weekly-progress.stories.js
@@ -0,0 +1,9 @@
+import "./weekly-progress.css";
+import weeklyProgress from "./weekly-progress.html";
+
+export default {
+  title: "Components/weekly-progress",
+  parameters: { layout: "centered" },
+};
+
+export const weeklyprogress = () => weeklyProgress;
