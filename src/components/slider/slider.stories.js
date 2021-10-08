8b78ce655b210925df3e1dfd61881f6e6736c400
“WebDevManuel” <donmanuel2005@web.de>
2021-10-08 14:23:16 +0200

    Add component slider

---
+++ src/components/slider/slider.stories.js
@@ -0,0 +1,9 @@
+import "./slider.css";
+import slider from "./slider.html";
+
+export default {
+  title: "Components/slider",
+  parameters: { layout: "centered" },
+};
+
+export const footSlider = () => slider;
