c609f4a15581f2d78a1fe43ac8aeab332164356f
markobrkovic <querry999@gmail.com>
2021-10-08 13:36:15 +0200

    Added component pros and cons

---
+++ src/components/pros-and-cons/pros-and-cons.stories.js
@@ -0,0 +1,9 @@
+import "./pros-and-cons.css";
+import prosAndCons from "./pros-and-cons.html";
+
+export default {
+  title: "Components/Pros And Cons of Fast Food",
+  parameters: { layout: "centered" },
+};
+
+export const prosAndConsOfFastFood = () => prosAndCons;
